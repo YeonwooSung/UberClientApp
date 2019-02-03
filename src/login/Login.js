f8e1f5f5b633464d45aa09c05cc39b0a2cd495f0
YeonwooSung <neos960518@gmail.com>
2019-02-04 02:37:38 +0900

    start implementing the log in page

---
+++ src/login/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.logoImage} source={require('../../assets/logo.png')} />
+                <TextInput style={styles.inputBox}
+                    placeholder="ID"
+                    placeholderTextColor="#1a3f95"
+                    selectionColor="#fff"
+                    keyboardType="email-address"
+                    onChangeText={(id) => this.setState({ id: id })}
+                    value={this.state.id}
+                    onSubmitEditing={() => this.password.focus()}
+                />
+                <TextInput style={styles.inputBox}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    placeholderTextColor="#1a3f95"
+                    onChangeText={(pw) => this.setState({ pw: pw })}
+                    value={this.state.pw}
+                    ref={(input) => this.password = input}
+                />
+                <TouchableOpacity style={styles.loginButtonBox} onPress={() => this.storeID()}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    logoImage: {
+        width: height / 6,
+        height: height / 6,
+        marginBottom: height / 10,
+        marginTop: height / 20
+    },
+    inputBox: {
+        width: width * 4 / 5,
+        height: height / 15,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        borderRadius: 25,
+        paddingHorizontal: 16,
+        fontSize: width / 25,
+        color: '#ffffff',
+        marginVertical: 5
+    },
+    loginButtonBox: {
+        width: width * 4 / 5,
+        height: height / 15,
+        backgroundColor: '#a8a9ad',
+        borderRadius: 25,
+        marginVertical: 10,
+        paddingVertical: 5,
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: width / 25,
+        fontWeight: '500',
+        color: "#ffffff",
+        textAlign: 'center'
+    }
+});
