e56292fa441fcb7aeb2ec9a65788cbcf5a07fbce
YeonwooSung <neos960518@gmail.com>
2019-02-07 07:37:24 +0900

    use stack navigator as a navigation

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+
 import Main from './src/Main';
+
+/* create stack navigator */
+const AppStackNavigator = createStackNavigator({
+  Main: { screen: Main },
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Main />
-    );
+    let Container = createAppContainer(AppStackNavigator);
+    return (<Container />);
   }
 }
