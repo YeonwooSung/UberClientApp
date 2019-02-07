f68976bab00fc102b985a4791e258af4f2c825df
YeonwooSung <neos960518@gmail.com>
2019-02-07 09:40:41 +0900

    implement the loading page

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { AsyncStorage } from 'react-native';
+import { 
+    AsyncStorage,
+    View,
+    Image,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
 
 import MainScreen from './container/Main';
 import LoginScreen from './container/Login';
 import SignupScreen from './container/Signup';
+
+
+const { width, height } = Dimensions.get('window');
 
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            screenName: 'Login'
+            screenName: 'Login',
+            isLoaded: false
         }
     }
 
@@ -27,6 +37,9 @@ export default class Main extends React.Component {
         if (id && pw) {
             this.setState({ screenName: 'Main' });
         }
+
+        //after finish checking, load the corresponding screen (Main screen or Log in Screen)
+        this.setState({ isLoaded: true });
     }
 
     componentDidMount = () => {
@@ -39,18 +52,41 @@ export default class Main extends React.Component {
     }
 
     render() {
-        let { screenName } = this.state;
+        let { screenName, isLoaded } = this.state;
 
         /* check the screen name to load the suitable screen */
-        if (screenName == 'Login') {
-            return (<LoginScreen navigateTo={this.changeScreen}></LoginScreen>);
-        } else if (screenName == 'Signup') {
-            return (<SignupScreen navigateTo={this.changeScreen}></SignupScreen>);
-        }
+        if (isLoaded) {
+            if (screenName == 'Login') {
+                return (<LoginScreen navigateTo={this.changeScreen}></LoginScreen>);
+            } else if (screenName == 'Signup') {
+                return (<SignupScreen navigateTo={this.changeScreen}></SignupScreen>);
+            }
 
-        return (
-            <MainScreen navigateTo={this.changeScreen}></MainScreen>
-        );
+            return (
+                <MainScreen navigateTo={this.changeScreen}></MainScreen>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Image style={styles.logoImage} source={require('../assets/logo.png')} />
+                </View>
+            );
+        }
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1a3f95',
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoImage: {
+        width: height / 6,
+        height: height / 6,
+        marginBottom: height / 10,
+        marginTop: height / 20
+    }
+});
