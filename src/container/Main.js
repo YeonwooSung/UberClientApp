de5051b150f76ec02446f03ff6d4c89dc688b836
YeonwooSung <neos960518@gmail.com>
2019-02-07 09:24:21 +0900

    display google map on the screen

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,12 +1,26 @@
-import React, { Component, PropTypes } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import { 
+    StyleSheet, 
+    View, 
+    Dimensions 
+} from 'react-native';
 import PropTypes from 'prop-types';
+
+import MapView from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
 
 export default class MainScreen extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            region: {
+                latitude: 56.335054,
+                longitude: -2.8063431,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
         }
     }
 
@@ -15,15 +29,33 @@ export default class MainScreen extends Component {
         navigateTo: PropTypes.func.isRequired
     }
 
+    onRegionChange(newRegion) {
+        this.setState({ region: newRegion });
+    }
+
     render() {
+        let { region } = this.state;
+        
         return (
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <MapView
+                    region={region}
+                    style={styles.mapContainer}
+                    onRegionChange={() => {this.onRegionChange()}}
+                />
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        justifyContent: 'center',
+    },
+    mapContainer: {
+        flex: 1,
+        width: width,
+        height: height,
     }
 });
