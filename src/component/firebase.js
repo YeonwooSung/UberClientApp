5d072df4eb6fc7db640b733fb4f54c184cc1f9a6
YeonwooSung <neos960518@gmail.com>
2019-02-08 20:36:00 +0900

    add comments

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -2,10 +2,10 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "<YOUR-API-KEY>",
-  authDomain: "<YOUR-AUTH-DOMAIN>",
-  databaseURL: "<YOUR-DATABASE-URL>",
-  storageBucket: "<YOUR-STORAGE-BUCKET>"
+  apiKey: "<YOUR-API-KEY>",               // Your API KEY
+  authDomain: "<YOUR-AUTH-DOMAIN>",       // Your authentication domain
+  databaseURL: "<YOUR-DATABASE-URL>",     // The URL of your DB
+  storageBucket: "<YOUR-STORAGE-BUCKET>"  // The storage bucket
 };
 
 firebase.initializeApp(firebaseConfig); //initialize the firebase app
