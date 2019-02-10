c04e761910835225fbf93277d527b5f9a87bcb99
YeonwooSung <neos960518@gmail.com>
2019-02-11 07:10:22 +0900

    improved the sign up screen

--- src/container/Signup.js
+++ src/container/Signup.js
@@ -11,8 +11,6 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const firebase = require('../component/firebase');
-
 const { width, height } = Dimensions.get('window');
 
 export default class SignupScreen extends React.Component {
@@ -20,27 +18,51 @@ export default class SignupScreen extends React.Component {
         super(props);
 
         this.state = {
+            email: '',
+            password: '',
             surname: '',
-            forename: ''
+            forename: '',
+            phoneNumber: ''
         }
     }
+
     static propTypes = {
         navigateTo: PropTypes.func.isRequired
     }
 
-    //TODO need to test this
+    storeUserInfo = async () => {
+        try {
+            const { email, password, surname, forename, phoneNumber } = this.state;
+
+            //await AsyncStorage.setItem('cs3301Uber@id', id);
+            //TODO store the list of user info in the local storage
+
+        } catch {
+            alert('failed to sign up');
+        }
+    }
+
+    /* This method checks if the user filled all text inputs */
     checkIfTheUserFilledAllTextInput = () => {
-        const { navigate } = this.props.navigateTo;
+        const navigate = this.props.navigateTo;
 
-        const {surname, forename} = this.state;
-
-        console.log(this.password);
+        const {email, password, surname, forename, phoneNumber} = this.state;
 
         if (surname !== '') {
             if (forename != '') {
-                //TODO firebase.storeUserInformation(email, password, forename, surname);
-                
-                navigate('Login');
+                if (phoneNumber !== '') {
+                    if (email !== '') {
+                        if (password !== '') {
+                            naviagate('Login');
+                        } else {
+                            alert('Please input password');
+                        }
+                    } else {
+                        alert('Please input your email address');
+                    }
+                } else {
+                    alert('Please input your phone number!');
+                }
             } else {
                 alert('Please input your forename');
             }
@@ -51,6 +73,7 @@ export default class SignupScreen extends React.Component {
 
     render() {
         const navigate = this.checkIfTheUserFilledAllTextInput;
+        const {navigateTo} = this.props;
         return (
             <View style={styles.container}>
                 <StatusBar
@@ -63,34 +86,51 @@ export default class SignupScreen extends React.Component {
                     placeholder="Surname"
                     placeholderTextColor="#1a3f95"
                     selectionColor="#fff"
-                    keyboardType="name-phone-pad" /* TODO need to test this */
+                    keyboardType="name-phone-pad"
                     onSubmitEditing={(input) => this.setState({surname: input})}
                 />
                 <TextInput style={styles.inputBox}
                     placeholder="Forename"
                     placeholderTextColor="#1a3f95"
                     selectionColor="#fff"
-                    keyboardType="name-phone-pad" /* TODO need to test this */
+                    keyboardType="name-phone-pad"
                     onSubmitEditing={(input) => this.setState({ forename: input })}
+                />
+                <TextInput style={styles.inputBox}
+                    placeholder="Phone Number"
+                    placeholderTextColor="#1a3f95"
+                    selectionColor="#fff"
+                    keyboardType="numeric"
+                    onSubmitEditing={(input) => this.setState({ phoneNumber: input })}
                 />
                 <TextInput style={styles.inputBox}
                     placeholder="Email"
                     placeholderTextColor="#1a3f95"
                     selectionColor="#fff"
                     keyboardType="email-address"
-                    onSubmitEditing={() => this.password.focus()}
+                    onSubmitEditing={
+                        (input) => {
+                            this.password.focus();
+                            this.setSate({email: input});
+                        }
+                    }
                 />
                 <TextInput style={styles.inputBox}
                     placeholder="Password"
                     secureTextEntry={true}
                     placeholderTextColor="#1a3f95"
-                    ref={(input) => this.password = input}
+                    ref={
+                        (input) => {
+                            this.password = input;
+                            this.setState({password: input});
+                        }
+                    }
                 />
-                <TouchableOpacity style={styles.buttonBox}>
-                    <Text style={styles.buttonText}>"Signup"</Text>
+                <TouchableOpacity style={styles.buttonBox} onPress={() => navigate()}>
+                    <Text style={styles.buttonText}>Signup</Text>
                 </TouchableOpacity>
                 <View style={styles.textContainer}>
-                    <TouchableOpacity onPress={() => navigate()}>
+                    <TouchableOpacity onPress={() => navigateTo('Login')}>
                         <Text style={styles.toLoginPageButton}> Go back to log in page </Text>
                     </TouchableOpacity>
                 </View>
