2d76ec20056da21cdef49ec6c14e75d692c4ec48
YeonwooSung <neos960518@gmail.com>
2019-02-12 12:45:50 +0900

    use Geolocation rather than navigator.geolocation

--- src/container/Main.js
+++ src/container/Main.js
@@ -8,6 +8,7 @@ import {
 import PropTypes from 'prop-types';
 
 import MapView from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const { width, height } = Dimensions.get('window');
 
@@ -36,7 +37,28 @@ export default class MainScreen extends React.Component {
 
     componentDidMount() {
         this.removeInfo();
+
         // get the current geolocation position
+        Geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+
+                this.setState({
+                    region: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA,
+                    }
+                });
+            },
+            (error) => {
+                // See error code charts below.
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+        /*
         navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({
@@ -51,18 +73,7 @@ export default class MainScreen extends React.Component {
             (error) => console.log(error.message),
             //TODO { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }, //need to test this..
         );
-        this.watchID = navigator.geolocation.watchPosition(
-            position => {
-                this.setState({
-                    region: {
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                        latitudeDelta: LATITUDE_DELTA,
-                        longitudeDelta: LONGITUDE_DELTA,
-                    }
-                });
-            }
-        );
+        */
     }
 
     static propTypes = {
