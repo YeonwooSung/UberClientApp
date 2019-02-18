eb5cfe116f72e55cb1ae72daeae4b533e9488af6
YeonwooSung <neos960518@gmail.com>
2019-02-18 16:10:43 +0900

    add request trip screen to the stack navigator

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
 import Main from './src/Main';
+import RequestTripScreen from './src/container/RequestTripScreen';
 
 /* create stack navigator */
 const AppStackNavigator = createStackNavigator({
   Main: { screen: Main },
+  Request: { screen: RequestTripScreen }
 });
 
 export default class App extends React.Component {
---
+++ src/container/RequestTripScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    View,
+    StyleSheet
+} from 'react-native';
+
+export default class RequestTripScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                {/**/}
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
