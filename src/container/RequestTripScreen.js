8def2cef0e93f779b634e492fa1d815bdf6f8029
YeonwooSung <neos960518@gmail.com>
2019-02-19 06:33:40 +0900

    start implementing request trip screen

--- src/container/RequestTripScreen.js
+++ src/container/RequestTripScreen.js
@@ -5,7 +5,27 @@ import {
 } from 'react-native';
 
 export default class RequestTripScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        destination: undefined,
+        availableDrivers: [],
+        driver: undefined
+    }
+
+    componentDidMount = () => {
+        const region = this.props.navigation.getParam('region', {latitude: 56.34026, longitude: -2.808796});
+        const availableDrivers = this.props.navigation.getParam('drivers',[]);
+        //TODO any more parameters?
+
+        this.setState({destination: region, availableDrivers: availableDrivers});
+    }
+
     render() {
+        let { destination } = this.state;
+
         return (
             <View style={styles.container}>
                 {/**/}
