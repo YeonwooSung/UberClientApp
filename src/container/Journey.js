b5fb8a79c39e05299dd904907c33ea4bb4c93ef8
YeonwooSung <neos960518@gmail.com>
2019-02-20 08:33:57 +0900

    fixed the typo

--- src/container/Journey.js
+++ src/container/Journey.js
@@ -7,7 +7,7 @@ import {
     Dimensions,
     Image
 } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView from 'react-native-maps';
 import uuidv1 from 'uuid/v1';
 import PropTypes from 'prop-types';
 
@@ -30,7 +30,42 @@ export default class JourneyScreen extends React.Component {
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
         },
+        coords: [],
+        isLoaded: false
     }
+
+
+    /**
+     * This method gets the direction of the polyline by using the google directions API.
+     */
+    getDirections = async (startLoc, destinationLoc) => {
+        try {
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return {
+                    latitude: point[0],
+                    longitude: point[1]
+                }
+            })
+            this.setState({ coords: coords })
+            return coords
+        } catch (error) {
+            return error
+        }
+    }
+
+
+    componentDidMount = (pickUp, destination) => {
+
+        // The parameters should be string that contains latitude and longitude
+        // i.e. "40.1884979, 29.061018"
+        this.getDirections(pickUp, destination);
+
+        this.setState({isLoaded: true});
+    }
+
 
     /**
      * Change the state of the MainScreen component when the region is changed.
@@ -41,7 +76,7 @@ export default class JourneyScreen extends React.Component {
     }
 
     render() {
-        const { region } = this.region;
+        const { region, coords, isLoaded } = this.state;
 
         return (
             <View style={styles.container}>
@@ -49,7 +84,15 @@ export default class JourneyScreen extends React.Component {
                     region={region}
                     style={styles.mapContainer}
                     onRegionChange={() => { this.onRegionChange() }}
-                ></MapView>
+                >
+                    {isLoaded &&
+                    <MapView.Polyline
+                        coordinates={coords}
+                        strokeWidth={2}
+                        strokeColor="blue" 
+                    />
+                    }
+                </MapView>
             </View>
         )
     }
