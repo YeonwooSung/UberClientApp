cd3f22eae50b99a893992a5e9f7adb48ee9364f2
YeonwooSung <neos960518@gmail.com>
2019-02-21 10:52:05 +0900

    implement the basic UI of CheckRequest screen

--- src/container/CheckRequest.js
+++ src/container/CheckRequest.js
@@ -40,22 +40,51 @@ export default class CheckRequestScreen extends React.Component {
 
         this.setState({
             time: time, 
-            driver: driver
+            driver: driver,
+            pickUpLocation: pickup,
+            destination: dest,
+            destinationGeolocation: destGeo
         });
         //----------------------------------------------------------------------------
     }
-    
+
+
+    // This method will start the journey when the user press the "confirm" button
+    startJourney = () => {
+        //
+    }
+
+
+    // This method will cancel the journey when the user press the "cancel" button
+    cancelRequest = () => {
+        //
+    }
 
 
     render () {
         let { time, driver, destination } = this.state;
 
+        let driverString = 'The driver that you selected is ' + driver.name;
+        let destinationString = 'Your destination: ' + destination;
+        let timeString = 'Journey time: ';
+
         //TODO need to calculate the "estimated time for journey"
+        let estimatedTime = 'Estimated time: ';
 
         return (
             <View style={styles.container}>
                 <Image style={styles.logoImage} source={require('../../assets/logo.png')} />
-                <Text></Text>
+                <Text>Please confirm your journey</Text>
+                <Text>{driverString}</Text>
+                <Text>{destinationString}</Text>
+                <Text>{timeString}</Text>
+                <Text>{estimatedTime}</Text>
+                <TouchableOpacity onPress={this.startJourney}>
+                    <Text>Confirm</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.cancelRequest}>
+                    <Text>Cancel</Text>
+                </TouchableOpacity>
             </View>
         )
     }
