42cb3d000a8500e4a264f765cb7170c46fe3765e
YeonwooSung <neos960518@gmail.com>
2019-02-26 07:53:56 +0900

    get rid of firebase API

--- src/component/key.js
+++ src/component/key.js
@@ -1,9 +1,3 @@
 module.exports.GOOGLE_API_KEYS = {
     Google_API_KEY: 'AIzaSyD8CEUO1jAFiFAD42WpwrFavJDRvF8ibw8'
 }
-
-module.exports.FIREBASE_KEYS = {
-    apiKey: "AIzaSyAvXt01rTgZJOce1sqFPdXywSgo6_Fpl1Y",        // Your API KEY
-    authDomain: "uberapp-cb7ba.firebaseapp.com",              // Your authentication domain
-    databaseURL: "https://uberapp-cb7ba.firebaseio.com/"      // The URL of your DB
-}
